ebc325d414a1ed22be7a43c26b02be5f8d4c6e6a
Gronstad <Megan.Gronstad1@T-Mobile.com>
2019-07-10 08:52:13 -0700

    Removed private keys

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     aws_table_name: 'Parking_Events',
     aws_remote_config: {
-      accessKeyId: 'AKIA525MKNFCNHNWMBPT',
-      secretAccessKey: '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',
+      accessKeyId: '01234567890123456789',
+      secretAccessKey: 'abcdefghijklmnopqrstuvwxyz1234567890abcd',
       region: 'us-west-2',
     }
   };
