be6acc406e260a9cbbd76d2e67408227bfb6d1a3
Gronstad <Megan.Gronstad1@T-Mobile.com>
2019-07-15 14:46:08 -0700

    Changes to README.md

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,11 @@
 module.exports = {
     aws_table_name: 'Parking_Events',
     aws_remote_config: {
-      accessKeyId: '01234567890123456789',
-      secretAccessKey: 'abcdefghijklmnopqrstuvwxyz1234567890abcd',
+        // User name: car_owner_3
+        // Do NOT push the following, these keys are private
+        // Not to be published online
+      accessKeyId: 'AKIA525MKNFCA3SPMPVX',
+      secretAccessKey: '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',
       region: 'us-west-2',
     }
   };
